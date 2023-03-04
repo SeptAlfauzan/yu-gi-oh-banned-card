dd33271ef3ea1ddbdd0c37c59809077ff1ff52c3
SeptAlfauzan <alfauzansepta@gmail.com>
2023-03-04 10:37:03 +0700

    change init navigation bar color changing by using code in SplashScreenActivity

--- app/src/main/java/com/example/yugioh25tcgcardsbanned/SplashScreenActivity.kt
+++ app/src/main/java/com/example/yugioh25tcgcardsbanned/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -12,6 +13,8 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        window.navigationBarColor = ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary_variant)
+
 
 
         Handler().postDelayed({
